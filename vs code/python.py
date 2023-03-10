00ceeda8720e6d02b4c805ad8e6c5099ee4b121a
sathish <sathishveldhi04@gmail.com>
2023-03-10 15:03:41 +0530

    first version

---
+++ vs code/python.py
@@ -0,0 +1,11 @@
+n=int(input())
+
+for i in range(1,n+1):
+    spaces=" "*(n-i)
+    stars="* "*i 
+    print(spaces+stars)
+for i in range(1,n):
+    spaces=" "*(i)
+    stars="* "*(n-i) 
+    print(spaces+stars)   
+
